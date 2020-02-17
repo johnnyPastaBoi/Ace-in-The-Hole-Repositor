28f9b83e5edf1da1c3898d8bce296112e32547e1
johnnyPastaBoi <61001834+johnnyPastaBoi@users.noreply.github.com>
2020-02-17 14:19:20 -0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div>
+        <a href="#"><img src="images/vectors/facebook.png"></a>
+        <a href="#"><img src="images/vectors/twitter.png"></a>
+        <a href="#"><img src="images/vectors/instagram.png"></a>
+    </div>
+
+
+    <ul>
+        <li> <a href="#">FAQ</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Register</a></li>
+    </ul>
+    <p>&copy; 2020 Ace in the Hole</p>
+</footer>
---
+++ includes/nav.php
@@ -0,0 +1,19 @@
+<nav>
+    <!--Div to add grid area to organize logo and hamburger menu-->
+    <div>
+        <!--Logo-->
+        <a href="../index.html"><img src="images/logos/logo_2.png" class="logo" alt="ace in the hole logo"></a>
+        <!--Hamburger Menu-->
+        <button class="hamburger hamburger--collapse" type="button">
+                <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+                </span>
+        </button>
+    </div>
+    <ul id="nav_ul">
+        <li><a href="#">Sign Up</a></li>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">About</a></li>
+    </ul>
+</nav>
